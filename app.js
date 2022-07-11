26df66ae1bfd2e2515e7a6ffe11b4858d0a6a926
Man1sh1409 <2019ucp1409@mnit.ac.in>
2022-07-12 02:09:47 +0530

    smartboard

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+app.use(express.static('public'));
+
+
+ let user=0;
+io.on('connection', (socket) => {
+    user++;
+  console.log('user number',user);
+  //received data 
+  socket.on("beginPath",(data)=>{
+    io.sockets.emit("beginPath",data);
+  })
+  socket.on("drawStroke",(data)=>{
+    io.sockets.emit("drawStroke",data);
+  })
+  socket.on("redoUndo",(data)=>{
+    io.sockets.emit("redoUndo",data);
+  })
+});
+
+server.listen(5500, () => {
+  console.log('listening on *:5500');
+});
